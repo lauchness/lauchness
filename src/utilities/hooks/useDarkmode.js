a00f6ffdc0564e75803175f40d9c3872814c180b
lauchness <lauchlanchisholm@gmail.com>
2020-03-30 13:33:02 -0300

    Updating useDarkmode to work in build

--- src/utilities/hooks/useDarkmode.js
+++ src/utilities/hooks/useDarkmode.js
@@ -3,16 +3,20 @@ import { useState, useEffect } from "react"
 const KEY = "lauchness-darkmode"
 
 function useDarkmode() {
-  const { localStorage, matchMedia } = window
-  const storagePreferenceJson = localStorage.getItem(KEY)
-  const storagePreference =
-    storagePreferenceJson && JSON.parse(storagePreferenceJson)
+  let initialState
+  if (typeof window !== "undefined") {
+    const { localStorage, matchMedia } = window
+    const storagePreferenceJson = localStorage && localStorage.getItem(KEY)
+    const storagePreference =
+      storagePreferenceJson && JSON.parse(storagePreferenceJson)
 
-  const mediaPreference = matchMedia("(prefers-color-scheme: dark)").matches
+    const mediaPreference = matchMedia("(prefers-color-scheme: dark)").matches
 
-  const [darkMode, setDarkMode] = useState(
-    storagePreference ? storagePreference.prefersDarkmode : mediaPreference
-  )
+    initialState = storagePreference
+      ? storagePreference.prefersDarkmode
+      : mediaPreference
+  }
+  const [darkMode, setDarkMode] = useState(initialState)
 
   const updateDarkmode = value => {
     const darkmodeJson = JSON.stringify({ prefersDarkmode: value })
