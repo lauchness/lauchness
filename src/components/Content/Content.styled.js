5526b185562efea57c4251c4a83db5803e24921a
lauchness <lauchlanchisholm@gmail.com>
2020-04-06 14:58:54 -0300

    Refer to LinkedIn for resume

--- src/components/Content/Content.styled.js
+++ src/components/Content/Content.styled.js
@@ -2,6 +2,6 @@ import styled from "@emotion/styled"
 
 export const StyledDiv = styled.div`
   max-width: ${({ maxWidth }) => `${maxWidth}px`};
-  margin: 0 auto;
+  margin: ${({ theme }) => theme.units.getGutter(2)} auto;
   padding: 0 ${props => props.theme.units.getGutter(2)};
 `
