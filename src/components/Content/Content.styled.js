e5b52b1aaee7d477790c982c64372299863d4ad5
lauchness <lauchlanchisholm@gmail.com>
2020-04-24 15:36:31 -0300

    Updating Charlie Parker article

--- src/components/Content/Content.styled.js
+++ src/components/Content/Content.styled.js
@@ -2,6 +2,9 @@ import styled from "@emotion/styled"
 
 export const StyledDiv = styled.div`
   max-width: ${({ maxWidth }) => `${maxWidth}px`};
-  margin: ${({ theme }) => theme.units.getGutter(2)} auto;
+  margin-top: ${({ theme }) => theme.units.getGutter(4)};
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: ${({ theme }) => theme.units.getGutter(8)};
   padding: 0 ${props => props.theme.units.getGutter(2)};
 `
